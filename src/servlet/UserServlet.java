f78e5dbe05b17c3b122b3da08905049bd669d393
Administrator <Administrator@A302-017>
2019-05-14 19:24:55 +0800

    daily update

--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -1,7 +1,7 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.lang.reflect.Method;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,20 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.User;
 import dao.UserDAO;
+import model.User;
 
 /**
- * @Servlet implementation class SignUpServlet
+ * @Servlet implementation class UserServlet
  */
-@WebServlet("/SignUpServlet")
-public class SignUpServlet extends HttpServlet {
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SignUpServlet() {
+    public UserServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,9 +31,35 @@ public class SignUpServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Method method = null;
+        String methodName = request.getParameter("method");
+        try {
+            method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, request, response);
+        } catch (Exception e) {
+            throw new RuntimeException("调用方法出错！");
+        }
+	}
+	
+	private void signIn(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		String confpass = request.getParameter("confpass");
+		System.out.println("# LoginAttempt， Username："+username+", Password："+password);
+		
+		UserDAO dao = new UserDAO();
+		int id = dao.confirmUser(username,password);
+		if(id > -1) {
+			response.sendRedirect("hello.jsp?username="+username+"&greeting=Welcome");
+		} else {
+			request.setAttribute("message", "账号或密码错误，请重新输入<br>");
+			request.getRequestDispatcher("SignIn.jsp").forward(request, response);
+		}
+	}
+	
+	private void signUp(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		//String confpass = request.getParameter("confpass");
 		boolean flag = true;
 		UserDAO dao = new UserDAO();
 		try {
